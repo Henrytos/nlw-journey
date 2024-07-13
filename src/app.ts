379e6feb56a9da0e3ed156989245c5bbfddc9989
Henry <franzhenry46@gmail.com>
2024-07-13 18:52:50 -0300

    chore: config orm prisma in project

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import fastify from "fastify";
+import { prisma } from "./utils/prisma";
 
 export const app = fastify();
+
+app.get("/trips", async () => {
+  const trips = await prisma.trip.findMany();
+
+  return { trips };
+});
